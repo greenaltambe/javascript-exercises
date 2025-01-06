d41410802882120e55a7de7407a943950fe5bbf3
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-01-06 08:19:10 +0000

    Complete remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const ans = [];
+    arr.forEach((item) => {
+        if(!args.includes(item)) {
+            ans.push(item);
+        }
+    });
+
+    return ans;
 };
 
 // Do not edit below this line
