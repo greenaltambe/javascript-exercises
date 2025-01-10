a2d7a74b94802d2a292a5ae1765c8ddd1886852f
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-01-10 10:50:37 +0000

    Complete palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const cleanString = str
+    .toLowerCase()
+    .split("")
+    .filter((character) => alphanumerical.includes(character))
+    .join();
+  const reversedString = cleanString.split("").reverse().join("");
+  return cleanString === reversedString;
 };
 
 // Do not edit below this line
